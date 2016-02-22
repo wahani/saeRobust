2dc8dabebe06d4ec002c72f1e4d3fbcd07584924
Sebastian <wahani@gmail.com>
2016-02-22 19:55:40 +0100

    using Hubers influence function

--- R/psiFunctions.R
+++ R/psiFunctions.R
@@ -10,16 +10,18 @@
 #' @export
 psiOne <- function(u, k = 1.345, deriv = FALSE){
     u <- as.numeric(u) # in the case that u comes from the Matrix package
+    if (deriv) return(as.numeric(abs(u) <= k))
 
-    if(deriv) return(as.numeric(abs(u) <= k))
+    # commented out is the influence function used by Tzavidis (2005) - M-Quantile
+    # sm <- median(abs(u)) / 0.6745
+    # w <- pmin(1, k / abs(u / sm))
+    # w * u
 
-    sm <- median(abs(u)) / 0.6745
-    w <- pmin(1, k / abs(u / sm))
-    w * u
+    u * pmin(1, k / abs(u))
 }
 
 #' @rdname psiFunctions
 #' @export
 getK <- function(k) {
-    2 * pnorm(k) - 1 - 2 * k * dnorm(k) + 2 * (k^2) * (1-pnorm(k))
+    2 * pnorm(k) - 1 - 2 * k * dnorm(k) + 2 * (k^2) * (1 - pnorm(k))
 }
